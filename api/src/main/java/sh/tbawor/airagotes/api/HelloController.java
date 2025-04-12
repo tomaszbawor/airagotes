1a303e889b81c8d1ab3be05a8d53fa9e232e11cb
Tomasz Bawor <bawortomasz@gmail.com>
2025-04-12 18:31:45 +0200

    chore: refactor

--- api/src/main/java/sh/tbawor/airagotes/api/HelloController.java
+++ api/src/main/java/sh/tbawor/airagotes/api/HelloController.java
@@ -1,29 +1,21 @@
 package sh.tbawor.airagotes.api;
 
 import org.springframework.ai.ollama.OllamaChatModel;
-import org.springframework.ai.ollama.api.OllamaApi;
-import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-  private final OllamaApi ollamaApi;
+  private final OllamaChatModel chatModel;
 
-  HelloController(OllamaApi ollamaApi) {
-    this.ollamaApi = ollamaApi;
+  HelloController(OllamaChatModel chatModel) {
+    this.chatModel = chatModel;
   }
 
   @GetMapping("/hello")
   public String hello() {
-    OllamaOptions options = new OllamaOptions();
-    options.setModel("gemma3:4b");
-
-    OllamaChatModel chatModel = OllamaChatModel.builder().ollamaApi(this.ollamaApi).defaultOptions(options).build();
-
     var response = chatModel.call("Hello, how are you?");
-
     return response;
   }
 }
--- api/src/main/java/sh/tbawor/airagotes/ollama/Configuration.java
+++ api/src/main/java/sh/tbawor/airagotes/ollama/Configuration.java
@@ -1,6 +1,8 @@
 package sh.tbawor.airagotes.ollama;
 
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.context.annotation.Bean;
 
 @org.springframework.context.annotation.Configuration
@@ -9,5 +11,18 @@ public class Configuration {
   @Bean
   public OllamaApi ollamaApi() {
     return new OllamaApi("http://localhost:11434");
+  }
+
+  @Bean
+  public OllamaChatModel chatModel(OllamaApi ollamaApi) {
+    OllamaOptions options = new OllamaOptions();
+    options.setModel("gemma3:4b");
+
+    OllamaChatModel chatModel = OllamaChatModel
+        .builder()
+        .ollamaApi(ollamaApi)
+        .defaultOptions(options).build();
+    return chatModel;
+
   }
 }
