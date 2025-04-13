653379ede4a6cb120d13a5811e938717ec4771c6
Tomasz Bawor <bawortomasz@gmail.com>
2025-04-13 22:49:33 +0200

    chore: get url for ollama from properties

--- api/src/main/java/sh/tbawor/airagotes/ollama/Configuration.java
+++ api/src/main/java/sh/tbawor/airagotes/ollama/Configuration.java
@@ -9,38 +9,41 @@ import org.springframework.ai.ollama.OllamaEmbeddingModel;
 import org.springframework.ai.ollama.api.OllamaApi;
 import org.springframework.ai.ollama.api.OllamaModel;
 import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
 
-    @Bean
-    public OllamaApi ollamaApi() {
-        // TODO: Get From Properties
-        return new OllamaApi("http://localhost:11434");
-    }
+  @Value("${spring.ai.ollama.base-url}")
+  public String ollamaUrl;
 
-    @Bean
-    public EmbeddingModel embeddingModel(OllamaApi ollamaApi) {
-        OllamaOptions ollamaOptions = OllamaOptions.builder().model(OllamaModel.NOMIC_EMBED_TEXT).build();
-        return OllamaEmbeddingModel.builder().ollamaApi(ollamaApi).defaultOptions(ollamaOptions).build();
-    }
+  @Bean
+  public OllamaApi ollamaApi() {
+    return new OllamaApi(ollamaUrl);
+  }
 
-    @Bean
-    public OllamaChatModel chatModel(OllamaApi ollamaApi) {
-        OllamaOptions options = new OllamaOptions();
-        options.setModel("gemma3:4b");
-        options.setTemperature(0.3); // Low Temperature to improve consistency and avoid hallucinations
+  @Bean
+  public EmbeddingModel embeddingModel(OllamaApi ollamaApi) {
+    OllamaOptions ollamaOptions = OllamaOptions.builder().model(OllamaModel.NOMIC_EMBED_TEXT).build();
+    return OllamaEmbeddingModel.builder().ollamaApi(ollamaApi).defaultOptions(ollamaOptions).build();
+  }
 
-        return OllamaChatModel
-                .builder()
-                .ollamaApi(ollamaApi)
-                .defaultOptions(options).build();
+  @Bean
+  public OllamaChatModel chatModel(OllamaApi ollamaApi) {
+    OllamaOptions options = new OllamaOptions();
+    options.setModel("gemma3:4b");
+    options.setTemperature(0.3); // Low Temperature to improve consistency and avoid hallucinations
 
-    }
+    return OllamaChatModel
+        .builder()
+        .ollamaApi(ollamaApi)
+        .defaultOptions(options).build();
 
-    @Bean
-    public ChatClient chatClient(ChatModel chatModel) {
-        return ChatClient.builder(chatModel).defaultAdvisors(new SimpleLoggerAdvisor()).build();
-    }
+  }
+
+  @Bean
+  public ChatClient chatClient(ChatModel chatModel) {
+    return ChatClient.builder(chatModel).defaultAdvisors(new SimpleLoggerAdvisor()).build();
+  }
 }
